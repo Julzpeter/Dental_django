93841d7805344d7f2586b1ce919ee129aad265af
Julzpeter <chepngetichjuliet@gmail.com>
2020-08-27 20:17:52 +0300

    creates email in settings, views and updates the contact.html for the message

--- dental/settings.py
+++ dental/settings.py
@@ -114,4 +114,20 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'
+
+
+# EMAIL_HOST = 'smtp.google.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'drj@gmail.com'
+# EMAIL_HOST_PASSWORD = 'drPassword'
+# EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False
+
+
+# python -m smtpd -n -c DebuggingServer localhost:1025
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
--- website/views.py
+++ website/views.py
@@ -1,22 +1,34 @@
 from django.shortcuts import render
-
+from django.core.mail import send_mail 
 
 def index(request):
     return render(request, 'index.html', {})
 
 
 def contact(request):
-    return render(request, 'contact.html', {})
+    if request.method == 'POST':
+        # do stuff
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
 
-    # if request.method == 'POST':
-    #     # do stuff
-    #     message_name = request.POST['message-name']
-    #     message_email = request.POST['message_email']
-    #     message = request.POST['message']
+        #send email
+        send_mail(
+            message_name,#subject
+            message,#message
+            message_email,#from email;
+            ['chepngetichjuliet@gmail.com'],#to email
+            fail_silently=False,
+        )
 
-        
-    # else:
-    #     return render(request, 'contact.html',{})
+        return render(request, 'contact.html', {'message_name': message_name})
+
+    else:
+        #return page
+        return render(request, 'contact.html', {})
+
+    
+    
 
         
         
